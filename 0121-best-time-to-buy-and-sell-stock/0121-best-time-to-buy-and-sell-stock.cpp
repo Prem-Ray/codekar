25d5c6264dd9e81ed2cebc5163783a3f78defcc4
Premanshu Ray <93930405+Prem-Ray@users.noreply.github.com>
2025-03-27 11:09:54 +0530

    Time: 0 ms (100%), Space: 97.5 MB (25.71%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,37 +1,13 @@
 class Solution {
 public:
-
-    void solvemaxprofit(vector<int>&prices , int&minPrice , int&maxprofit,int i) {
-
-        //base case 
-        if(i>=prices.size()) {
-            return ;
-        }
-
-        //ek case solve karo 
-        minPrice = min(minPrice , prices[i]) ;
-        maxprofit =max(maxprofit , prices[i]-minPrice) ;
-
-        solvemaxprofit(prices,minPrice,maxprofit,i+1) ;
-
-    }
-
     int maxProfit(vector<int>& prices) {
-
-        // int maxprofit=0 ;
-        // int mini=INT_MAX ;
-
-        // for(int i=0 ; i<prices.size() ; i++) {
-        //     mini=min(mini,prices[i]) ;
-        //     maxprofit=max(maxprofit,prices[i]-mini) ;
-        // }
-
-        int minPrice = INT_MAX ;
-        int maxprofit = INT_MIN ;
-        int i=0 ;
-
-        solvemaxprofit(prices,minPrice,maxprofit,i) ;
-
-        return maxprofit ;
+        int minPrice = prices[0] ;
+        int maxProfit = 0 ;
+        for(int i=0 ; i<prices.size()  ; i++){
+            int profit = prices[i]-minPrice ;
+            maxProfit = max(maxProfit,profit) ;
+            minPrice = min(minPrice,prices[i]) ;
+        }
+        return maxProfit ;
     }
 };
