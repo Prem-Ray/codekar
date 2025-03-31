71e93dac92c05673160b63232f0bc957237dcbf6
Premanshu Ray <93930405+Prem-Ray@users.noreply.github.com>
2025-03-31 11:10:46 +0530

    Time: 3 ms (11.47%), Space: 18.7 MB (53.23%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,79 +1,63 @@
 class Solution {
 public:
-
-  /*  void colSet(vector<vector<int>>& matrix , int j) {
-        for(int i=0 ; i<matrix.size() ; i++) {
-            if(matrix[i][j]!=0) {
-                matrix[i][j]='k' ;
-            }
-        }
-    }
-
-    void rowSet(vector<vector<int>>& matrix , int i) {
-        for(int j=0 ; j<matrix[0].size() ; j++) {
-            if(matrix[i][j]!=0) {
-                matrix[i][j]='k' ;
-            }
-        }
-    } */
-    
     void setZeroes(vector<vector<int>>& matrix) {
-     /*   for(int i=0 ; i<matrix.size() ; i++) {
-            for(int j=0 ; j<matrix[0].size() ; j++) {
-                if(matrix[i][j]==0) {
-                    //colset elements marking 
-                    colSet(matrix,j) ;
-                    //rowset elements marking
-                    rowSet(matrix,i) ;
+        // for row size
+        int m = matrix.size() ; ;
+        // row matrix--> matrix[i][0]
+
+        // for column size
+        int n = matrix[0].size();
+        // column matrix--> matrix[0][j] 
+
+        // for first column observation
+        int col0 = 1 ;
+
+        for(int i=0 ; i<m ; i++){
+            for(int j=0 ; j<n ; j++){
+                if(matrix[i][j] == 0){
+                    // check it is you first column or not
+                    if(j!=0){
+                        // set your col matrix index 0
+                        matrix[0][j] = 0 ;
+                        // set your row matrix index 0
+                        matrix[i][0] = 0 ;
+                    }else{
+                        // set your col0 to 0
+                        col0 = 0 ;
+                        // set your row matrix index 0
+                        matrix[i][0] = 0 ;
+                    }
                 }
             }
         }
 
-        for(int i=0 ; i<matrix.size() ; i++) {
-            for(int j=0 ; j<matrix[0].size() ; j++) {
-                if(matrix[i][j]=='k') {
-                    matrix[i][j]=0 ;
+
+        // traverse without row and column matrix 
+        for(int i=1 ; i<m ; i++){
+            for(int j=1 ; j<n ; j++){
+                if(matrix[i][j] != 0){
+                // check their corresponding row and column matrix index if any is 0 or not
+                    if(matrix[0][j]==0 || matrix[i][0]==0){
+                        matrix[i][j] = 0 ;
+                    }
                 }
             }
-        } */
+        }
 
-        int col0=1 ;
+        // column matrix traverse for convert into 0
+        if(matrix[0][0]==0){
+            for(int j=0; j<n ; j++){
+                matrix[0][j] = 0 ;
+            }
+        }
+
+        // row matrix traverse for convert into 0 
+        if(col0 == 0){
+            for(int i=0 ; i<m ; i++){
+                matrix[i][0] = 0 ;
+            }
+        }
+
         
-        for(int i=0 ; i<matrix.size() ; i++) {
-            for(int j=0 ; j<matrix[0].size() ; j++) {
-                if(matrix[i][j]==0) {
-                    matrix[i][0]=0 ;
-                    if(j==0) {
-                        col0=0 ;
-                    }
-                    else {
-                        matrix[0][j]=0 ;
-                    }
-                }
-            }
-        }
-
-        for(int i=1 ; i<matrix.size() ; i++) {
-            for(int j=1 ; j<matrix[0].size() ; j++) {
-                if(matrix[i][j]!=0) {
-                    if(matrix[0][j]==0 || matrix[i][0]==0) {
-                        matrix[i][j]=0 ;
-                    }
-                }
-            }
-        }
-
-        if(matrix[0][0]==0) {
-            for(int j=0 ; j<matrix[0].size() ; j++) {
-                matrix[0][j]=0 ;
-            }
-        }
-
-        if(col0==0) {
-            for(int i=0 ; i<matrix.size() ; i++) {
-                matrix[i][0]=0 ;
-            }
-        }
     }
-
 };
