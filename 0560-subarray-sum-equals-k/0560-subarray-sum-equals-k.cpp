5a870086ecce7e06aa2e03e816abbe53fd16557b
Premanshu Ray <93930405+Prem-Ray@users.noreply.github.com>
2025-04-01 12:47:32 +0530

    Time: 46 ms (60.76%), Space: 51 MB (23.74%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,15 +2,21 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int,int>mpp ;
-        mpp[0] =  1 ;
-        int presum = 0 ;
+        // initialize the map with assuming there have a prefix sum 0 with it's occurence 1
+        mpp[0] = 1 ;
+
+        int prefixsum = 0 ;
         int count = 0 ;
 
         for(int i=0 ; i<nums.size() ; i++){
-            presum += nums[i] ;
-            int remaining = presum-k ;
+            // calculate the prefix sum
+            prefixsum += nums[i] ;
+            // calculate the remaining sum
+            int remaining = prefixsum-k ;
+            // if remaining not exists so it's value is 0 , if exists take it's value    and add with count to get the total occurences
             count += mpp[remaining] ;
-            mpp[presum] += 1 ;
+            // update the prefix sum with incrementing ocuurence in the map
+            mpp[prefixsum] +=1 ;
         }
         return count ;
     }
