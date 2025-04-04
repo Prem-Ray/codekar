d4dcc07e408c40c104569e311b9390490decff96
Premanshu Ray <93930405+Prem-Ray@users.noreply.github.com>
2025-04-04 11:38:48 +0530

    Time: 1 ms (23.65%), Space: 19.8 MB (84.68%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+
         int n = nums.size() ;
         vector<int>list ; 
-        int j = 0 ;
+        
         for(int i=0 ; i<n ; i++){
+        
+            // check your current item is in the list or not
             bool found = false ;
             for(auto item : list){
                 if(item == nums[i]){
@@ -14,15 +17,23 @@ public:
             }
 
             int count = 0 ;
+
+            // if youur current item present in list so leave it
+            // if not present in the list so then you find the item in array 
             if(!found){
                 for(int j=0 ; j<n ; j++){
+                    // if your item is match then count
                     if(nums[j] == nums[i]){
                        count++ ;
                     }
                 }
+
+            // if your current item appearance more than n/3 , then push it into list
                 if(count>n/3){
                     list.push_back(nums[i]) ;
                 }
+
+                // if your list already have 2 element so nothing to insert leave it
                 if(list.size()==2){
                     break ;
                 }
