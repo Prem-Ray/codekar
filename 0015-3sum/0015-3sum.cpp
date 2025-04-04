43927c82185f42b9d7acfb0eb601b67b17a2e5e7
Premanshu Ray <93930405+Prem-Ray@users.noreply.github.com>
2025-04-05 02:09:40 +0530

    Time: 55 ms (41.75%), Space: 31.1 MB (31.02%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         
         // Optimal Approach
-        set<vector<int>>st ;
+        vector<vector<int>>ans ;
         sort(nums.begin(),nums.end()) ;
         for(int i=0 ; i<nums.size() ; i++){
 
@@ -25,18 +25,13 @@ public:
                 }
                 else{
                     vector<int>temp = {nums[i],nums[j],nums[k]} ;
-                    st.insert(temp) ;
+                    ans.push_back(temp) ;
                     j++ ;
                     k-- ;
                     while(j<k && nums[j] == nums[j-1]) j++ ;
                     while(j<k && nums[k] == nums[k+1]) k-- ;
                 }
             }
-        }
-
-        vector<vector<int>>ans ;
-        for(auto item:st){
-            ans.push_back(item) ;
         }
 
         return ans ;
