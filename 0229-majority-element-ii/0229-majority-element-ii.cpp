37c5f0b6cd85304b2d685ee9c8cc4bb1dbf9889e
Premanshu Ray <93930405+Prem-Ray@users.noreply.github.com>
2025-04-04 12:37:53 +0530

    Time: 3 ms (15.69%), Space: 20 MB (9.08%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,67 +2,70 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
 
-        // int n = nums.size() ;
-        // map<int,int>mpp ;
-        // vector<int>list ;
-
-        
-        // for(int i=0 ; i<n ; i++){
-        //     mpp[nums[i]]++ ;
-
-        //     if(mpp[nums[i]]>n/3){
-        //         list.push_back(nums[i]) ;
-        //     }
-
-        //     if(list.size() == ceil(n/3)){
-        //         break ;
-        //     }
-
-        // }
-
-        // return list ;
-
-
-
-
-
         int n = nums.size() ;
-        vector<int>list ; 
+        map<int,int>mpp ;
+        vector<int>list ;
+
+        int mini = (n/3)+1 ;
+
         
         for(int i=0 ; i<n ; i++){
-        
-            // check your current item is in the list or not
-            bool found = false ;
-            for(auto item : list){
-                if(item == nums[i]){
-                    found = true ;
-                    break ;
-                }
+            
+            mpp[nums[i]]++ ;
+
+            if(mpp[nums[i]] == mini){
+                list.push_back(nums[i]) ;
             }
 
-            int count = 0 ;
-
-            // if youur current item present in list so leave it
-            // if not present in the list so then you find the item in array 
-            if(!found){
-                for(int j=0 ; j<n ; j++){
-                    // if your item is match then count
-                    if(nums[j] == nums[i]){
-                       count++ ;
-                    }
-                }
-
-            // if your current item appearance more than n/3 , then push it into list
-                if(count>n/3){
-                    list.push_back(nums[i]) ;
-                }
-
-                // if your list already have 2 element so nothing to insert leave it
-                if(list.size()== ceil(2)){
-                    break ;
-                }
+            if(list.size() == 2){
+                break ;
             }
+
         }
+
         return list ;
+
+
+
+
+
+        // int n = nums.size() ;
+        // vector<int>list ; 
+        
+        // for(int i=0 ; i<n ; i++){
+        
+        //     // check your current item is in the list or not
+        //     bool found = false ;
+        //     for(auto item : list){
+        //         if(item == nums[i]){
+        //             found = true ;
+        //             break ;
+        //         }
+        //     }
+
+        //     int count = 0 ;
+
+        //     // if youur current item present in list so leave it
+        //     // if not present in the list so then you find the item in array 
+        //     if(!found){
+        //         for(int j=0 ; j<n ; j++){
+        //             // if your item is match then count
+        //             if(nums[j] == nums[i]){
+        //                count++ ;
+        //             }
+        //         }
+
+        //     // if your current item appearance more than n/3 , then push it into list
+        //         if(count>n/3){
+        //             list.push_back(nums[i]) ;
+        //         }
+
+        //         // if your list already have 2 element so nothing to insert leave it
+        //         if(list.size()== ceil(2)){
+        //             break ;
+        //         }
+        //     }
+        // }
+        // return list ;
     }
 };
