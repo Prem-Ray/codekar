53a6bbebb16ff4f37bf68d8e627c3ca5f380164a
Premanshu Ray <93930405+Prem-Ray@users.noreply.github.com>
2025-04-05 02:07:10 +0530

    Time: 88 ms (23.44%), Space: 40.4 MB (18.21%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -28,8 +28,8 @@ public:
                     st.insert(temp) ;
                     j++ ;
                     k-- ;
-                    // while(j<k && nums[j] == nums[j-1]) j++ ;
-                    // while(j<k && nums[k] == nums[k+1]) k-- ;
+                    while(j<k && nums[j] == nums[j-1]) j++ ;
+                    while(j<k && nums[k] == nums[k+1]) k-- ;
                 }
             }
         }
