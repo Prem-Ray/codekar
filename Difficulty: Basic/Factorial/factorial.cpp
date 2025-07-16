4d1dedb2bee2584ebdb391cbc5707d4954fad6db
Premanshu Ray <93930405+Prem-Ray@users.noreply.github.com>
2025-07-17 01:23:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Factorial/factorial.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+  
+   
+    void factorial(int n) {
+        // code here
+        int ans = 1;
+        for (int i = 2; i <= n; i++) {
+            ans = ans * (i);
+        }
+        cout << ans << endl;
+    }
+};
