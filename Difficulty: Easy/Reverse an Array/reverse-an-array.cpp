b49615a332fa421e120ce86af181ab520e29d7ea
Premanshu Ray <93930405+Prem-Ray@users.noreply.github.com>
2025-07-17 01:34:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse an Array/reverse-an-array.cpp
+++ Difficulty: Easy/Reverse an Array/reverse-an-array.cpp
@@ -1,64 +1,13 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-  
-    void reverseArray(vector<int> &arr,int l=0 , int r=-1) {
+    void fxn(vector<int>&arr,int i){
+        if(i==arr.size()/2) return ;
+        swap(arr[i],arr[arr.size()-i-1]) ;
+        fxn(arr,i+1) ;
+    }
+    void reverseArray(vector<int> &arr) {
         // code here
-        
-        // Brute Force Approach
-        // int l=0 ;
-        // int e=arr.size()-1 ;
-        
-        // while(l<e){
-        //     swap(arr[l],arr[e]) ;
-        //     l++ ;
-        //     e-- ;
-        // }
-        
-        // Optimal Approach
-        if(r==-1){
-            r=arr.size()-1 ;
-        }
-        if(l>=r) {
-            return ;
-        }
-        
-        swap(arr[l],arr[r]) ;
-        reverseArray(arr,l+1,r-1) ;
-        
+        int i=0 ;
+        fxn(arr,i) ;
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        ob.reverseArray(arr);
-        for (int i = 0; i < arr.size(); i++) {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
