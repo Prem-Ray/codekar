54a122c3b5d1bbc9fe80286f02b54ecc37846986
Premanshu Ray <93930405+Prem-Ray@users.noreply.github.com>
2025-07-17 21:24:33 +0530

    Time: 9 ms (49.39%), Space: 7.6 MB (98.47%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-        /* base case , for 0th index number is 0 and 1th index number is 1 , so
-        return  the number for that case */
-        if(n<=1) return n ;
-        int lastnum = fib(n-1) ;
-        int secondlastnum = fib(n-2) ;
-        return lastnum + secondlastnum ;
+        if(n==0) return 0 ;
+        if(n==1) return 1 ;
+        return fib(n-1)+fib(n-2) ;
     }
 };
