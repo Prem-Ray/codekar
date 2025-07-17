6132c1f96f0ee23c41dac8e987edc25fcf06ba5a
Premanshu Ray <93930405+Prem-Ray@users.noreply.github.com>
2025-07-17 21:12:40 +0530

    Time: 3 ms (32.09%), Space: 10.2 MB (28.19%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,36 +1,30 @@
 class Solution {
-private:
-    bool checkindex(char ch){
-        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z') || (ch>='0' && ch<='9')){
+public:
+    bool checkPointer(char ch){
+        if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z') || (ch>='0' && ch<='9')){
             return true ;
         }
         return false ;
     }
 
-
-public:
-    
-    bool isPalindrome(string s,int l=0, int e=-1) {
-        string temp = "" ;
-        for(int i=0 ; i<s.size() ; i++){
-            if(checkindex(s[i])){
+    bool isPalindrome(string s) {
+        // for check point
+        string temp="" ;
+        for(int i=0 ; i<=s.size()-1 ; i++){
+            if(checkPointer(s[i])){
                 temp.push_back(tolower(s[i])) ;
             }
         }
-        if(e==-1){
-            e=temp.size()-1 ;
-        }
-
-        while(l<e){
-            if(temp[l] != temp[e]){
+        
+        int l=0 , h=temp.size()-1 ;
+        while(l<h){
+            if(temp[l]!=temp[h]){
                 return false ;
             }
             l++ ;
-            e-- ;
+            h-- ;
         }
         return true ;
+
     }
-    
 };
-   
- 
