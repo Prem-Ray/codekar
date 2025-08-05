176af4acc404f7a8ff513ff4430425b3c77aa025
Premanshu Ray <93930405+Prem-Ray@users.noreply.github.com>
2025-08-05 12:49:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome String/palindrome-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    bool isPalindrome(string& s) {
+        // code here
+        int start = 0 ;
+        int end = s.size()-1 ;
+        
+        while(start<end){
+            if(s[start]!=s[end]) return false ;
+            start++ ;
+            end-- ;
+        }
+        return true ;
+    }
+};
