a1f2f74391d9616616c798e82a669aa5274424af
Premanshu Ray <93930405+Prem-Ray@users.noreply.github.com>
2025-09-05 11:33:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome String/palindrome-string.cpp
+++ Difficulty: Easy/Palindrome String/palindrome-string.cpp
@@ -2,18 +2,14 @@ class Solution {
   public:
     bool isPalindrome(string& s) {
         // code here
-       int start = 0 ;
-       int end = s.size()-1 ;
-       
-       while(start<end){
-        //   this condition match means this is not palindrome
-           if(s[start] != s[end]) return false ;
-           
-            //   if items match 
+        int start = 0 ;
+        int end = s.size()-1 ;
+        
+        while(start<end){
+            if(s[start]!=s[end]) return false ;
             start++ ;
             end-- ;
-       }
-       
-       return true ;
+        }
+        return true ;
     }
 };
