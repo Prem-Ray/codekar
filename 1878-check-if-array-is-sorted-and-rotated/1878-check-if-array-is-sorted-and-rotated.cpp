53a0a0a17207e8d28cc07150b1d285e5e063bbce
Premanshu Ray <93930405+Prem-Ray@users.noreply.github.com>
2025-09-15 21:17:55 +0530

    Time: 0 ms (100%), Space: 11.9 MB (5.74%) - LeetHub

---
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    void reversethepart(vector<int>&nums, int start,int end){
+        while(start<end){
+            swap(nums[start],nums[end]) ;
+            start++ ;
+            end-- ;
+        }
+    }
+
+    bool check(vector<int>& nums) {
+
+        vector<int>arr(nums) ;
+        sort(arr.begin(),arr.end()) ;
+
+        int count=0 ;
+        for(int i=0 ; i<nums.size()-1 ; i++){
+            if(nums[i] > nums[i+1]){
+                count=i+1 ;
+            }
+        }
+
+        reversethepart(nums,0,count-1) ;
+        reversethepart(nums,count,nums.size()-1) ;
+        reversethepart(nums,0,nums.size()-1) ;
+
+        for(int i=0 ; i<arr.size() ; i++){
+            if(arr[i] != nums[i]){
+                return false ;
+            }
+        }
+
+        return true ;
+    }
+};
