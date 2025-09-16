32f386d86b673061973e6b3f040b30fdc3be4456
Premanshu Ray <93930405+Prem-Ray@users.noreply.github.com>
2025-09-16 19:52:06 +0530

    Time: 0 ms (100%), Space: 26.1 MB (67.61%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-    
-    int start=0 ;
-    int end=nums.size()-1 ;
-    int mid;
-    int ans ;
-  
-    while(start<=end) {
-    
-        mid=start+(end-start)/2 ;
+        
+        int n=nums.size() ;
 
-        if(start==end) {
-            return nums[start] ;
-        }
+        // if only one element
+        if(n == 1) return nums[0] ;
 
-        else if(mid%2==0) {
-            if(nums[mid]==nums[mid+1]) {
-                start=mid+2 ;
+        // if your first element is your unique element
+        if(nums[0] != nums[1]) return nums[0] ;
+
+        // if your last element is the unique element
+        if(nums[n-1] != nums[n-2]) return nums[n-1] ;
+
+        int start=0 ;
+        int end=nums.size()-1 ;
+
+        while(start <= end){
+
+            int mid = start+(end-start)/2 ;
+
+            // we found the unique element
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+                return nums[mid] ;
             }
-            else {
-                ans=nums[mid] ;
-                end=mid ;
+            // stand on left part
+            else if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1])){
+                start = mid+1 ;
+            }
+            // stand on right part
+            else{
+                end = mid-1 ;
             }
         }
-        else  {
-            if(nums[mid]==nums[mid+1]) {
-                end=mid-1 ;
-            }
-            else {
-                start=mid+1 ;
-            }
-        }
-    }
-     return ans ;
+        return -1 ;
     }
 };
